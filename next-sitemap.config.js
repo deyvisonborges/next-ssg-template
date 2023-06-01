e79f697e7df468b28878e313bfd05df83efda6d8
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-05-31 22:52:14 -0300

    add script to build sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://example.com', // FIXME: Change to the production URL
+  generateRobotsTxt: true
+}
